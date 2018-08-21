a2f4268b6537db553a377fc15ee4c6ee4f99caaa
Vincent Royer <vroyer@strapdata.com>
2018-08-21 23:22:08 +0200

    Fix ready_probe when running cassandra only

--- ready-probe.sh
+++ ready-probe.sh
@@ -5,7 +5,7 @@
 #set -x
 
 if [[ $($CASSANDRA_HOME/bin/nodetool status 2>/dev/null | grep "$POD_IP" | awk '{ print $1 }') == "UN" ]]; then
-  if [[ $(curl -XGET  "http://$POD_IP:9200/_cat/nodes?h=ip" 2>/dev/null | grep "$POD_IP" ) == "$POD_IP" ]]; then
+  if [[ "$CASSANDRA_DAEMON" == "org.apache.cassandra.service.CassandraDaemon" ]] || [[ $(curl -XGET  "http://$POD_IP:9200/_cat/nodes?h=ip" 2>/dev/null | grep "$POD_IP" ) == "$POD_IP" ]]; then
      exit 0;
   else 
      if [[ $DEBUG ]]; then
