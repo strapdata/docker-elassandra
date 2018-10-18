de144caf9eb423610a5faa92de9dde2b7c66e21c
dbarthe <barthelemy.delemotte@gmail.com>
2018-10-18 17:39:08 +0200

    compute ready-probe default POD_IP

--- ready-probe.sh
+++ ready-probe.sh
@@ -7,8 +7,19 @@ if [[ $DEBUG ]]; then
   set -x
 fi
 
+_ip_address() {
+	# scrape the first non-localhost IP address of the container
+	# in Swarm Mode, we often get two IPs -- the container IP, and the (shared) VIP, and the container IP should always be first
+	ip address | awk '
+		$1 == "inet" && $NF != "lo" {
+			gsub(/\/.+$/, "", $2)
+			print $2
+			exit
+		}
+	'
+}
 
-if [[ $(nodetool status | grep ${POD_IP:=localhost}) == *"UN"* ]]; then
+if [[ $(nodetool status | grep ${POD_IP:-(_ip_address)}) == *"UN"* ]]; then
   if [[ "${CASSANDRA_DAEMON:-org.apache.cassandra.service.CassandraDaemon}" == "org.apache.cassandra.service.CassandraDaemon" ]] || \
      curl -s -XGET "http://localhost:9200/" 2>&1 >/dev/null; then
      exit 0;
