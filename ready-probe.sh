e80ff186f0e7722229675214647412ac291fc0ba
vroyer <vroyer@strapdata.com>
2019-01-06 22:11:59 +0100

    simple ready-probe.sh checking listen ports

--- ready-probe.sh
+++ ready-probe.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 #
-# Check for elassandra availability.
+# Check for elassandra LISTEN ports
 #
 
-if [[ $DEBUG ]]; then
-  set -x
-fi
-
-_ip_address() {
-	# scrape the first non-localhost IP address of the container
-	# in Swarm Mode, we often get two IPs -- the container IP, and the (shared) VIP, and the container IP should always be first
-	ip address | awk '
-		$1 == "inet" && $NF != "lo" {
-			gsub(/\/.+$/, "", $2)
-			print $2
-			exit
-		}
-	'
-}
-
-POD_IP=${POD_IP:-$(_ip_address)}
-
-cqlsh -e "SELECT * FROM system.local" ${POD_IP}
-if [ $? -eq 0 ]; then
-  if [[ "${CASSANDRA_DAEMON:-org.apache.cassandra.service.CassandraDaemon}" == "org.apache.cassandra.service.CassandraDaemon" ]] || \
-     curl -s -XGET "http://${POD_IP}:9200/" 2>&1 >/dev/null --max-time ${READINESS_PROBE_TIMEOUT:-30}; then
-     exit 0;
-  else 
-     if [[ $DEBUG ]]; then
-        echo "Elasticsearch not UP";
-     fi
-     exit 2;
-  fi
+if [[ "${CASSANDRA_DAEMON:-org.apache.cassandra.service.CassandraDaemon}" == "org.apache.cassandra.service.CassandraDaemon" ]]; then
+   exec grep "00000000:2352" /proc/net/tcp
 else
-  if [[ $DEBUG ]]; then
-    echo "Cassandra not UP";
-  fi
-  exit 1;
+   exec grep "00000000:23F0" /proc/net/tcp
 fi
