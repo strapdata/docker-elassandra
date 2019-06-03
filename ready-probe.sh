da3219b64423e5f0814e92e24222bc064f77ca0d
vroyer <vroyer@strapdata.com>
2019-06-03 12:57:03 +0200

    Customizable listen ports

--- ready-probe.sh
+++ ready-probe.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
-# Check for elassandra LISTEN ports
-#
+# Check for elassandra service ports
+# $1=cassandra CQL port, default=9042
+# $2=elasticsearch http port, default=9200
 
 if [[ "${CASSANDRA_DAEMON:-org.apache.cassandra.service.CassandraDaemon}" == "org.apache.cassandra.service.CassandraDaemon" ]]; then
-   exec grep "00000000:2352" /proc/net/tcp
+   exec grep "00000000:$(printf '%X' ${1:-9042})" /proc/net/tcp
 else
-   exec grep "00000000:23F0" /proc/net/tcp
+   exec grep "00000000:$(printf '%X' ${2:-9200})" /proc/net/tcp
 fi
